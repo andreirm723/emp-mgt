4c797e812c9ce1e19844e3a83259984aae4fe663
Wilmsy <masangcaywilma@gmail.com>
2024-05-30 14:56:55 +0800

    hello

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -8,11 +8,11 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Employee Management</h1>
+            <h1>Employee Management -- WSM</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item active">Employee Management // WILMS</li>
+              <li class="breadcrumb-item active">Employee Management</li>
             </ol>
           </div>
         </div>
